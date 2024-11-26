ea13efbe79752c358573867ee5ae03f08bf124be
Jakub Tomczyk <qeeu.que@gmail.com>
2024-11-27 00:15:52 +0100

    docs doxygen

--- Polygon.cpp
+++ Polygon.cpp
@@ -1,112 +1,165 @@
+/**
+ * @file Polygon.cpp
+ * @brief Implementation of the Polygon class, representing a 2D polygon with rendering and transformation functionalities.
+ */
+
 #include "Point2D.h"
 #include "Polygon.h"
 #include "PrimitiveRenderer.h"
 #include "math.h"
 #include <cmath>
 #include <vector>
-Polygon::Polygon(Uint8 r, Uint8 g, Uint8 b, Uint8 a, std::vector<Point2D>& points) : r{ r }, g{ g }, b{ b }, a{ a },points { points },xc{0.0},yc{0.0},A{0.0}
+
+ /**
+  * @brief Constructs a Polygon object with specified color and points.
+  *
+  * @param r Red component of the polygon's color.
+  * @param g Green component of the polygon's color.
+  * @param b Blue component of the polygon's color.
+  * @param a Alpha (transparency) component of the polygon's color.
+  * @param points Reference to a vector of Point2D objects defining the vertices of the polygon.
+  */
+Polygon::Polygon(Uint8 r, Uint8 g, Uint8 b, Uint8 a, std::vector<Point2D>& points)
+    : r{ r }, g{ g }, b{ b }, a{ a }, points{ points }, xc{ 0.0 }, yc{ 0.0 }, A{ 0.0 }
 {
-	calculateCenter();
+    calculateCenter();
 }
 
+/**
+ * @brief Draws the polygon on the screen.
+ */
 void Polygon::draw()
 {
-	PrimitiveRenderer::draw_polygon(r,g,b,a,points);
-
+    PrimitiveRenderer::draw_polygon(r, g, b, a, points);
 }
 
+/**
+ * @brief Fills the polygon with a specified color and boundary color.
+ *
+ * @param fillColor The fill color for the polygon.
+ * @param boundryColor The boundary color for the polygon.
+ */
 void Polygon::fill(SDL_Color fillColor, SDL_Color boundryColor)
 {
-	PrimitiveRenderer::RiteracyjnyBoundaryFill(Engine::getInstance()->getRenderer(),xc,yc,fillColor,boundryColor);
+    PrimitiveRenderer::RiteracyjnyBoundaryFill(Engine::getInstance()->getRenderer(), xc, yc, fillColor, boundryColor);
 }
 
+/**
+ * @brief Calculates the geometric center of the polygon.
+ */
 void Polygon::calculateCenter()
 {
-	double sumX = 0;
-	double sumY = 0;
-	for (int i = 0; i < points.size(); i++)
-	{
-		sumX += points[i].getCoordinates('X');
-		sumY += points[i].getCoordinates('Y');
-	}
-	xc = sumX / points.size();
-	yc = sumY / points.size();
+    double sumX = 0;
+    double sumY = 0;
+    for (int i = 0; i < points.size(); i++)
+    {
+        sumX += points[i].getCoordinates('X');
+        sumY += points[i].getCoordinates('Y');
+    }
+    xc = sumX / points.size();
+    yc = sumY / points.size();
 }
 
+/**
+ * @brief Translates (moves) the polygon by a specified offset.
+ *
+ * @param tx Offset along the x-axis.
+ * @param ty Offset along the y-axis.
+ */
 void Polygon::translate(int tx, int ty)
 {
-	for(int i = 0; i < points.size(); i++)
-	{
-		int x = points[i].getCoordinates('X');
-		int y = points[i].getCoordinates('Y');
-		int x2 = x + tx;
-		int y2 = y + ty;
-		
-		// Przesunieie o wektor
-		points[i].setCoordinates(x2, y2);
-		calculateCenter();
-	}
-	
+    for (int i = 0; i < points.size(); i++)
+    {
+        int x = points[i].getCoordinates('X');
+        int y = points[i].getCoordinates('Y');
+        int x2 = x + tx;
+        int y2 = y + ty;
 
+        points[i].setCoordinates(x2, y2);
+    }
+    calculateCenter();
 }
 
+/**
+ * @brief Rotates the polygon around its center by a specified angle.
+ *
+ * @param alfa Angle of rotation in degrees.
+ */
 void Polygon::rotate(double alfa)
 {
-	double alfaRad = alfa * M_PI / 180.0;
+    double alfaRad = alfa * M_PI / 180.0;
 
-	for(int i = 0; i < points.size(); i++)
-	{
-		int x = points[i].getCoordinates('X');
-		int y = points[i].getCoordinates('Y');
-		int x2 = x * cos(alfaRad) - y * sin(alfaRad);
-		int y2 = x * sin(alfaRad) + y * cos(alfaRad);
-		
-		points[i].setCoordinates(x2, y2);
+    for (int i = 0; i < points.size(); i++)
+    {
+        int x = points[i].getCoordinates('X');
+        int y = points[i].getCoordinates('Y');
+        int x2 = x * cos(alfaRad) - y * sin(alfaRad);
+        int y2 = x * sin(alfaRad) + y * cos(alfaRad);
 
-	}
-	calculateCenter();
+        points[i].setCoordinates(x2, y2);
+    }
+    calculateCenter();
 }
 
+/**
+ * @brief Rotates the polygon around a specified point by a given angle.
+ *
+ * @param alfa Angle of rotation in degrees.
+ * @param x X-coordinate of the rotation point.
+ * @param y Y-coordinate of the rotation point.
+ */
 void Polygon::rotatePoint(double alfa, int x, int y)
 {
-	double alfaRad = alfa * M_PI / 180.0;
-	
-	for(int i = 0; i < points.size(); i++)
-	{
-		int x1 = points[i].getCoordinates('X');
-		int y1 = points[i].getCoordinates('Y');
-		int x2 = x + (x1 - x) * cos(alfaRad) - (y1 -y) * sin(alfaRad);
-		int y2 = y + (x1 - x) * sin(alfaRad) + (y1 - y) * cos(alfaRad);
+    double alfaRad = alfa * M_PI / 180.0;
 
-		points[i].setCoordinates(x2, y2);
-	}
-	calculateCenter();
+    for (int i = 0; i < points.size(); i++)
+    {
+        int x1 = points[i].getCoordinates('X');
+        int y1 = points[i].getCoordinates('Y');
+        int x2 = x + (x1 - x) * cos(alfaRad) - (y1 - y) * sin(alfaRad);
+        int y2 = y + (x1 - x) * sin(alfaRad) + (y1 - y) * cos(alfaRad);
+
+        points[i].setCoordinates(x2, y2);
+    }
+    calculateCenter();
 }
 
+/**
+ * @brief Scales the polygon uniformly by a specified factor.
+ *
+ * @param k Scaling factor. A value greater than 1 enlarges the polygon, while a value less than 1 shrinks it.
+ */
 void Polygon::scale(double k)
 {
-	for(int i = 0; i < points.size(); i++)
-	{
-		int x = points[i].getCoordinates('X');
-		int y = points[i].getCoordinates('Y');
-		int x2 = x * k;
-		int y2 = y * k;
+    for (int i = 0; i < points.size(); i++)
+    {
+        int x = points[i].getCoordinates('X');
+        int y = points[i].getCoordinates('Y');
+        int x2 = x * k;
+        int y2 = y * k;
 
-		points[i].setCoordinates(x2, y2);
-	}
-	calculateCenter();
+        points[i].setCoordinates(x2, y2);
+    }
+    calculateCenter();
 }
 
+/**
+ * @brief Scales the polygon around a specified point by a given factor.
+ *
+ * @param k Scaling factor. A value greater than 1 enlarges the polygon, while a value less than 1 shrinks it.
+ * @param x X-coordinate of the scaling point.
+ * @param y Y-coordinate of the scaling point.
+ */
 void Polygon::scalePoint(double k, int x, int y)
 {
-	for(int i = 0; i < points.size(); i++)
-	{
-		int x1 = points[i].getCoordinates('X');
-		int y1 = points[i].getCoordinates('Y');
-		int x2 = x1 * k + (1 - k) * x;
-		int y2 = y1 * k + (1 - k) * y;
+    for (int i = 0; i < points.size(); i++)
+    {
+        int x1 = points[i].getCoordinates('X');
+        int y1 = points[i].getCoordinates('Y');
+        int x2 = x1 * k + (1 - k) * x;
+        int y2 = y1 * k + (1 - k) * y;
 
-		points[i].setCoordinates(x2, y2);
-	}
-	calculateCenter();
+        points[i].setCoordinates(x2, y2);
+    }
+    calculateCenter();
 }
--- Polygon.h
+++ Polygon.h
@@ -1,22 +1,105 @@
+/**
+ * @file Polygon.h
+ * @brief Declaration of the Polygon class, representing a 2D polygon with rendering and transformation functionalities.
+ */
+
 #pragma once
 #include "Point2D.h"
 #include "ShapeObject.h"
 #include <vector>
+
+ /**
+  * @class Polygon
+  * @brief Represents a 2D polygon defined by a list of points, with functionalities for rendering and transformations.
+  *
+  * The Polygon class provides methods to draw, fill, and transform a polygon, such as translation, rotation, and scaling.
+  * It also includes functionality to calculate the center of the polygon.
+  */
 class Polygon : public ShapeObject
 {
 public:
-	Polygon(Uint8 r, Uint8 g, Uint8 b, Uint8 a, std::vector<Point2D>& points);
-	void draw() override;
-	void fill(SDL_Color fillColor, SDL_Color boundryColor) override;
-	void translate(int tx, int ty) override;
-	void rotate(double alfa) override;
-	void rotatePoint(double alfa, int x, int y) override;
-	void scale(double k) override;
-	void scalePoint(double k, int x, int y) override;
-	void calculateCenter();
-private:
-	Uint8 r, g, b, a;
-	double xc, yc, A = 0.0;
-	std::vector<Point2D>& points;
-};
+    /**
+     * @brief Constructs a Polygon object with specified color and points.
+     *
+     * @param r Red component of the polygon's color.
+     * @param g Green component of the polygon's color.
+     * @param b Blue component of the polygon's color.
+     * @param a Alpha (transparency) component of the polygon's color.
+     * @param points Reference to a vector of Point2D objects defining the vertices of the polygon.
+     */
+    Polygon(Uint8 r, Uint8 g, Uint8 b, Uint8 a, std::vector<Point2D>& points);
 
+    /**
+     * @brief Draws the polygon on the screen.
+     *
+     * This method renders the polygon's outline using the appropriate rendering functions.
+     */
+    void draw() override;
+
+    /**
+     * @brief Fills the polygon with a specified color and boundary color.
+     *
+     * @param fillColor The fill color for the polygon.
+     * @param boundryColor The boundary color for the polygon.
+     */
+    void fill(SDL_Color fillColor, SDL_Color boundryColor) override;
+
+    /**
+     * @brief Translates (moves) the polygon by a specified offset.
+     *
+     * @param tx Offset along the x-axis.
+     * @param ty Offset along the y-axis.
+     */
+    void translate(int tx, int ty) override;
+
+    /**
+     * @brief Rotates the polygon around its center by a specified angle.
+     *
+     * @param alfa Angle of rotation in degrees.
+     */
+    void rotate(double alfa) override;
+
+    /**
+     * @brief Rotates the polygon around a specified point by a given angle.
+     *
+     * @param alfa Angle of rotation in degrees.
+     * @param x X-coordinate of the rotation point.
+     * @param y Y-coordinate of the rotation point.
+     */
+    void rotatePoint(double alfa, int x, int y) override;
+
+    /**
+     * @brief Scales the polygon uniformly by a specified factor.
+     *
+     * @param k Scaling factor. A value greater than 1 enlarges the polygon, while a value less than 1 shrinks it.
+     */
+    void scale(double k) override;
+
+    /**
+     * @brief Scales the polygon around a specified point by a given factor.
+     *
+     * This method adjusts the position of all vertices and scales the polygon accordingly.
+     *
+     * @param k Scaling factor. A value greater than 1 enlarges the polygon, while a value less than 1 shrinks it.
+     * @param x X-coordinate of the scaling point.
+     * @param y Y-coordinate of the scaling point.
+     */
+    void scalePoint(double k, int x, int y) override;
+
+    /**
+     * @brief Calculates the geometric center of the polygon.
+     *
+     * This method computes the center of the polygon based on its vertices.
+     */
+    void calculateCenter();
+
+private:
+    Uint8 r; /**< Red component of the polygon's color. */
+    Uint8 g; /**< Green component of the polygon's color. */
+    Uint8 b; /**< Blue component of the polygon's color. */
+    Uint8 a; /**< Alpha (transparency) component of the polygon's color. */
+    double xc; /**< X-coordinate of the polygon's center. */
+    double yc; /**< Y-coordinate of the polygon's center. */
+    double A = 0.0; /**< Area of the polygon, used for calculating the center. */
+    std::vector<Point2D>& points; /**< Reference to a vector of points defining the polygon's vertices. */
+};
