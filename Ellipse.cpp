4118f229eca318557d9664eb745fe3cff817ed32
Jakub Tomczyk <qeeu.que@gmail.com>
2024-11-26 23:57:47 +0100

    docs doxygen

--- Ellipse.cpp
+++ Ellipse.cpp
@@ -1,77 +1,120 @@
+/**
+ * @file Ellipse.cpp
+ * @brief Implementation of the Ellipse class, representing a 2D ellipse with rendering and transformation capabilities.
+ */
+
 #include "Ellipse.h"
 #include "PrimitiveRenderer.h"
 #include <cmath>
-Ellipse::Ellipse(Uint8 r, Uint8 g, Uint8 b, Uint8 an, unsigned int xc, unsigned int yc, unsigned int R, unsigned int R2) : r{r},g{g},b{b},an{an},ellipseCenter{xc, yc}, R{R}, R2{R2}, rotationAngle{0.0}
+
+ /**
+  * @brief Constructs an Ellipse object with specified color, center position, and radii.
+  *
+  * @param r Red component of the ellipse's color.
+  * @param g Green component of the ellipse's color.
+  * @param b Blue component of the ellipse's color.
+  * @param an Alpha (transparency) component of the ellipse's color.
+  * @param xc X-coordinate of the ellipse's center.
+  * @param yc Y-coordinate of the ellipse's center.
+  * @param R Radius along the x-axis (horizontal radius).
+  * @param R2 Radius along the y-axis (vertical radius).
+  */
+Ellipse::Ellipse(Uint8 r, Uint8 g, Uint8 b, Uint8 an, unsigned int xc, unsigned int yc, unsigned int R, unsigned int R2)
+    : r{ r }, g{ g }, b{ b }, an{ an }, ellipseCenter{ xc, yc }, R{ R }, R2{ R2 }, rotationAngle{ 0.0 }
 {
 
 }
+
+/**
+ * @brief Draws the ellipse on the screen.
+ *
+ * Uses the PrimitiveRenderer to render the ellipse with its current properties.
+ */
 void Ellipse::draw()
 {
-	PrimitiveRenderer::draw_ellipse(r, g, b, an, rotationAngle, ellipseCenter.getCoordinates('X'), ellipseCenter.getCoordinates('Y'), R, R2);
+    PrimitiveRenderer::draw_ellipse(r, g, b, an, rotationAngle, ellipseCenter.getCoordinates('X'), ellipseCenter.getCoordinates('Y'), R, R2);
 }
+
+/**
+ * @brief Fills the ellipse with a specified color and boundary color.
+ *
+ * Uses a boundary fill algorithm to fill the ellipse with a color while respecting its boundaries.
+ *
+ * @param fillColor The fill color for the ellipse.
+ * @param boundryColor The boundary color for the ellipse.
+ */
 void Ellipse::fill(SDL_Color fillColor, SDL_Color boundryColor)
 {
-	PrimitiveRenderer::RiteracyjnyBoundaryFill(Engine::getInstance()->getRenderer(),ellipseCenter.getCoordinates('X'), ellipseCenter.getCoordinates('Y'), fillColor, boundryColor);
+    PrimitiveRenderer::RiteracyjnyBoundaryFill(Engine::getInstance()->getRenderer(), ellipseCenter.getCoordinates('X'), ellipseCenter.getCoordinates('Y'), fillColor, boundryColor);
 }
 
+/**
+ * @brief Translates (moves) the ellipse by a specified offset.
+ *
+ * @param tx Offset along the x-axis.
+ * @param ty Offset along the y-axis.
+ */
 void Ellipse::translate(int tx, int ty)
 {
-	ellipseCenter.setCoordinates(ellipseCenter.getCoordinates('X') + tx, ellipseCenter.getCoordinates('Y') - ty);
+    ellipseCenter.setCoordinates(ellipseCenter.getCoordinates('X') + tx, ellipseCenter.getCoordinates('Y') - ty);
 }
 
+/**
+ * @brief Rotates the ellipse around its center by a specified angle.
+ *
+ * Currently, this function updates the rotation angle in radians but does not modify the ellipse's points.
+ *
+ * @param alfa Angle of rotation in degrees.
+ */
 void Ellipse::rotate(double alfa)
 {
-	double alfaRad = alfa * M_PI / 180.0;
-	/* float a = 0.0; */
-	
-	/* int x = R * cos(a); */
-	/* int y = R2 * sin(a); */
-	/* int xc = ellipseCenter.getCoordinates('X'); */
-	/* int yc = ellipseCenter.getCoordinates('Y'); */
-
-	/* int x2 = x * cos(rotationAngle) - y * sin(rotationAngle); */
-	/* int y2 = x * sin(rotationAngle) + y * cos(rotationAngle); */
-
-	/* int x1_point = xc + x; */
-	/* int x2_point = xc - x; */
-	/* int y1_point = yc + y; */
-	/* int y2_point = yc - y; */
-
-	/* ellipseCenter.setCoordinates(x1_point, y1_point); */
-	/* ellipseCenter.setCoordinates(x2_point, y1_point); */
-	/* ellipseCenter.setCoordinates(x1_point, y2_point); */
-	/* ellipseCenter.setCoordinates(x2_point, y2_point); */
-	
-	rotationAngle +=alfaRad;
-	std::cout << "Current rotation angle (radians): " << rotationAngle << std::endl;
+    double alfaRad = alfa * M_PI / 180.0;
+    rotationAngle += alfaRad;
 }
 
+/**
+ * @brief Rotates the ellipse around a specified point by a given angle.
+ *
+ * @param alfa Angle of rotation in degrees.
+ * @param x X-coordinate of the rotation point.
+ * @param y Y-coordinate of the rotation point.
+ *
+ * @note This function is not yet implemented.
+ */
 void Ellipse::rotatePoint(double alfa, int x, int y)
 {
-	double alfaRad = alfa * M_PI / 180.0;
-
-	int xc = ellipseCenter.getCoordinates('X');
-	int yc = ellipseCenter.getCoordinates('Y');
-	
-	/* rotationAngle += alfaRad; */
-
+    // Implementation pending.
 }
 
+/**
+ * @brief Scales the ellipse uniformly by a specified factor.
+ *
+ * @param k Scaling factor. A value greater than 1 enlarges the ellipse, while a value less than 1 shrinks it.
+ */
 void Ellipse::scale(double k)
 {
-	R *= k;
-	R2 *= k;
+    R *= k;
+    R2 *= k;
 }
 
+/**
+ * @brief Scales the ellipse around a specified point by a given factor.
+ *
+ * This method adjusts both the ellipse's center position and its radii.
+ *
+ * @param k Scaling factor. A value greater than 1 enlarges the ellipse, while a value less than 1 shrinks it.
+ * @param x X-coordinate of the scaling point.
+ * @param y Y-coordinate of the scaling point.
+ */
 void Ellipse::scalePoint(double k, int x, int y)
 {
-	int x_point = ellipseCenter.getCoordinates('X');
-	int y_point = ellipseCenter.getCoordinates('Y');
+    int x_point = ellipseCenter.getCoordinates('X');
+    int y_point = ellipseCenter.getCoordinates('Y');
 
-	int x2_point = x_point * k + (1 - k) * x;
-	int y2_point = y_point * k + (1 - k) * y;
+    int x2_point = x_point * k + (1 - k) * x;
+    int y2_point = y_point * k + (1 - k) * y;
 
-	ellipseCenter.setCoordinates(x2_point, y2_point);
-	R *= k;
-	R2 *= k;
+    ellipseCenter.setCoordinates(x2_point, y2_point);
+    R *= k;
+    R2 *= k;
 }
--- Ellipse.h
+++ Ellipse.h
@@ -1,20 +1,98 @@
+/**
+ * @file Ellipse.h
+ * @brief Declaration of the Ellipse class, representing a 2D ellipse with rendering and transformation functionalities.
+ */
+
 #pragma once
 #include "ShapeObject.h"
+
+ /**
+  * @class Ellipse
+  * @brief Represents a 2D ellipse with functionalities for rendering and transformations.
+  *
+  * The Ellipse class provides methods to draw, fill, and transform an ellipse, such as translation, rotation, and scaling.
+  */
 class Ellipse : public ShapeObject
 {
 public:
-	Ellipse(Uint8 r, Uint8 g, Uint8 b, Uint8 an, unsigned int xc, unsigned int yc, unsigned int R, unsigned int R2);
-	void draw() override;
-	void fill(SDL_Color fillColor, SDL_Color boundryColor) override;
-	void translate(int tx, int ty) override;
-	void rotate(double alfa) override;
-	void rotatePoint(double alfa, int x, int y) override;
-	void scale(double k) override;
-	void scalePoint(double k, int x, int y) override;
-private:
-	Uint8 r,  g,  b,  an;
-	unsigned int R, R2;
-	Point2D ellipseCenter;
-	double rotationAngle;
-};
+    /**
+     * @brief Constructs an Ellipse object with specified color, center position, and radii.
+     *
+     * @param r Red component of the ellipse's color.
+     * @param g Green component of the ellipse's color.
+     * @param b Blue component of the ellipse's color.
+     * @param an Alpha (transparency) component of the ellipse's color.
+     * @param xc X-coordinate of the ellipse's center.
+     * @param yc Y-coordinate of the ellipse's center.
+     * @param R Horizontal radius (along the x-axis) of the ellipse.
+     * @param R2 Vertical radius (along the y-axis) of the ellipse.
+     */
+    Ellipse(Uint8 r, Uint8 g, Uint8 b, Uint8 an, unsigned int xc, unsigned int yc, unsigned int R, unsigned int R2);
 
+    /**
+     * @brief Draws the ellipse on the screen.
+     *
+     * This method renders the ellipse's outline using the appropriate rendering functions.
+     */
+    void draw() override;
+
+    /**
+     * @brief Fills the ellipse with a specified color and boundary color.
+     *
+     * @param fillColor The fill color for the ellipse.
+     * @param boundryColor The boundary color for the ellipse.
+     */
+    void fill(SDL_Color fillColor, SDL_Color boundryColor) override;
+
+    /**
+     * @brief Translates (moves) the ellipse by a specified offset.
+     *
+     * @param tx Offset along the x-axis.
+     * @param ty Offset along the y-axis.
+     */
+    void translate(int tx, int ty) override;
+
+    /**
+     * @brief Rotates the ellipse around its center by a specified angle.
+     *
+     * @param alfa Angle of rotation in degrees.
+     */
+    void rotate(double alfa) override;
+
+    /**
+     * @brief Rotates the ellipse around a specified point by a given angle.
+     *
+     * @param alfa Angle of rotation in degrees.
+     * @param x X-coordinate of the rotation point.
+     * @param y Y-coordinate of the rotation point.
+     */
+    void rotatePoint(double alfa, int x, int y) override;
+
+    /**
+     * @brief Scales the ellipse uniformly by a specified factor.
+     *
+     * @param k Scaling factor. A value greater than 1 enlarges the ellipse, while a value less than 1 shrinks it.
+     */
+    void scale(double k) override;
+
+    /**
+     * @brief Scales the ellipse around a specified point by a given factor.
+     *
+     * This method adjusts both the ellipse's center position and its radii.
+     *
+     * @param k Scaling factor. A value greater than 1 enlarges the ellipse, while a value less than 1 shrinks it.
+     * @param x X-coordinate of the scaling point.
+     * @param y Y-coordinate of the scaling point.
+     */
+    void scalePoint(double k, int x, int y) override;
+
+private:
+    Uint8 r;              /**< Red component of the ellipse's color. */
+    Uint8 g;              /**< Green component of the ellipse's color. */
+    Uint8 b;              /**< Blue component of the ellipse's color. */
+    Uint8 an;             /**< Alpha (transparency) component of the ellipse's color. */
+    unsigned int R;       /**< Horizontal radius (along the x-axis) of the ellipse. */
+    unsigned int R2;      /**< Vertical radius (along the y-axis) of the ellipse. */
+    Point2D ellipseCenter; /**< Center point of the ellipse. */
+    double rotationAngle; /**< Current rotation angle of the ellipse in radians. */
+};
