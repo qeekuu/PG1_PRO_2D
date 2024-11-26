c89216e9508de15d72da3807ef1e30af08979d6d
Jakub Tomczyk <qeeu.que@gmail.com>
2024-11-27 00:04:16 +0100

    docs doxygen

--- LineSegment.cpp
+++ LineSegment.cpp
@@ -1,16 +1,40 @@
+/**
+ * @file LineSegment.cpp
+ * @brief Implementation of the LineSegment class.
+ */
+
 #include "LineSegment.h"
 #include "Point2D.h"
-LineSegment::LineSegment(int x,int y,int x2,int y2) : point(x,y),point2(x2,y2)
+
+ /**
+  * @brief Constructs a LineSegment object with specified start and end points.
+  *
+  * @param x X-coordinate of the start point.
+  * @param y Y-coordinate of the start point.
+  * @param x2 X-coordinate of the end point.
+  * @param y2 Y-coordinate of the end point.
+  */
+LineSegment::LineSegment(int x, int y, int x2, int y2) : point(x, y), point2(x2, y2)
 {
 
 }
 
+/**
+ * @brief Gets the start point of the line segment.
+ *
+ * @return Reference to the start Point2D object.
+ */
 Point2D& LineSegment::getStart()
 {
-	return point;
+    return point;
 }
 
+/**
+ * @brief Gets the end point of the line segment.
+ *
+ * @return Reference to the end Point2D object.
+ */
 Point2D& LineSegment::getEnd()
 {
-	return point2;
+    return point2;
 }
--- LineSegment.h
+++ LineSegment.h
@@ -1,14 +1,46 @@
+/**
+ * @file LineSegment.h
+ * @brief Declaration of the LineSegment class, representing a line segment defined by two points.
+ */
+
 #pragma once
 #include "Point2D.h"
 #include <iostream>
+
+ /**
+  * @class LineSegment
+  * @brief Represents a 2D line segment defined by a start and end point.
+  *
+  * The LineSegment class provides methods to access the start and end points of the segment.
+  */
 class LineSegment
 {
 public:
-	LineSegment(int x, int y, int x2, int y2);
-	Point2D& getStart();
-	Point2D& getEnd();
+    /**
+     * @brief Constructs a LineSegment object with specified start and end points.
+     *
+     * @param x X-coordinate of the start point.
+     * @param y Y-coordinate of the start point.
+     * @param x2 X-coordinate of the end point.
+     * @param y2 Y-coordinate of the end point.
+     */
+    LineSegment(int x, int y, int x2, int y2);
+
+    /**
+     * @brief Gets the start point of the line segment.
+     *
+     * @return Reference to the start Point2D object.
+     */
+    Point2D& getStart();
+
+    /**
+     * @brief Gets the end point of the line segment.
+     *
+     * @return Reference to the end Point2D object.
+     */
+    Point2D& getEnd();
+
 private:
-	Point2D point,point2;
+    Point2D point;  /**< Start point of the line segment. */
+    Point2D point2; /**< End point of the line segment. */
 };
-
-
