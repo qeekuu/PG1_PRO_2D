dd61fa51cc2ebca232d1ac0c95454e33785b109c
Jakub Tomczyk <qeeu.que@gmail.com>
2024-11-27 00:13:52 +0100

    docs doxygen

--- Point2D.cpp
+++ Point2D.cpp
@@ -1,39 +1,72 @@
+/**
+ * @file Point2D.cpp
+ * @brief Implementation of the Point2D class, representing a point in 2D space.
+ */
+
 #include "Point2D.h"
 #include <iostream>
 
-Point2D::Point2D(unsigned int x,unsigned  int y) : x{ x }, y{ y }
+ /**
+  * @brief Constructs a Point2D object with specified x and y coordinates.
+  *
+  * @param x Initial x-coordinate of the point.
+  * @param y Initial y-coordinate of the point.
+  */
+Point2D::Point2D(unsigned int x, unsigned int y) : x{ x }, y{ y }
 {
 
 }
 
+/**
+ * @brief Retrieves the x or y coordinate based on the specified character.
+ *
+ * @param sign Character indicating which coordinate to retrieve ('X' for x-coordinate, 'Y' for y-coordinate).
+ * @return The value of the requested coordinate. Returns -1 if the sign is invalid.
+ */
 int Point2D::getCoordinates(char sign)
 {
-	switch (sign)
-	{
-	case 'X':
-		return x;
-		break;
-	case 'Y':
-		return y;
-		break;
-	default:
-		return -1;
-		break;
-	}
+    switch (sign)
+    {
+    case 'X':
+        return x;
+        break;
+    case 'Y':
+        return y;
+        break;
+    default:
+        return -1;
+        break;
+    }
 }
 
+/**
+ * @brief Sets the x and y coordinates of the point.
+ *
+ * @param x New x-coordinate of the point.
+ * @param y New y-coordinate of the point.
+ */
 void Point2D::setCoordinates(unsigned int x, unsigned int y)
 {
-	this->x = x;
-	this->y = y;
+    this->x = x;
+    this->y = y;
 }
 
+/**
+ * @brief Sets the x-coordinate of the point.
+ *
+ * @param x New x-coordinate of the point.
+ */
 void Point2D::setCoordinatesX(unsigned int x)
 {
-	this->x = x;
+    this->x = x;
 }
 
+/**
+ * @brief Sets the y-coordinate of the point.
+ *
+ * @param y New y-coordinate of the point.
+ */
 void Point2D::setCoordinatesY(unsigned int y)
 {
-	this->y = y;
+    this->y = y;
 }
--- Point2D.h
+++ Point2D.h
@@ -1,15 +1,59 @@
+/**
+ * @file Point2D.h
+ * @brief Declaration of the Point2D class, representing a point in 2D space.
+ */
+
 #pragma once
 #include <iostream>
 
+ /**
+  * @class Point2D
+  * @brief Represents a point in 2D space with x and y coordinates.
+  *
+  * The Point2D class provides methods for retrieving and modifying the coordinates of a 2D point.
+  */
 class Point2D
 {
 public:
-	int getCoordinates(char sign);
-	void setCoordinates(unsigned int x, unsigned int y);
-	void setCoordinatesX(unsigned int x);
-	void setCoordinatesY(unsigned int y);
-	Point2D(unsigned int x,unsigned int y);
-private:
-	unsigned int x, y;
-};
+    /**
+     * @brief Retrieves the x or y coordinate based on the specified character.
+     *
+     * @param sign Character indicating which coordinate to retrieve ('X' for x-coordinate, 'Y' for y-coordinate).
+     * @return The value of the requested coordinate.
+     */
+    int getCoordinates(char sign);
 
+    /**
+     * @brief Sets the x and y coordinates of the point.
+     *
+     * @param x New x-coordinate of the point.
+     * @param y New y-coordinate of the point.
+     */
+    void setCoordinates(unsigned int x, unsigned int y);
+
+    /**
+     * @brief Sets the x-coordinate of the point.
+     *
+     * @param x New x-coordinate of the point.
+     */
+    void setCoordinatesX(unsigned int x);
+
+    /**
+     * @brief Sets the y-coordinate of the point.
+     *
+     * @param y New y-coordinate of the point.
+     */
+    void setCoordinatesY(unsigned int y);
+
+    /**
+     * @brief Constructs a Point2D object with specified x and y coordinates.
+     *
+     * @param x Initial x-coordinate of the point.
+     * @param y Initial y-coordinate of the point.
+     */
+    Point2D(unsigned int x, unsigned int y);
+
+private:
+    unsigned int x; /**< The x-coordinate of the point. */
+    unsigned int y; /**< The y-coordinate of the point. */
+};
