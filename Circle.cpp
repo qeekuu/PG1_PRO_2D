52a527c6af7771e5482669fbfabfedab72290e51
Jakub Tomczyk <qeeu.que@gmail.com>
2024-11-26 22:49:19 +0100

    docs doxygen

--- Circle.cpp
+++ Circle.cpp
@@ -1,65 +1,122 @@
+/**
+ * @file Circle.cpp
+ * @brief Implementation of the Circle class, representing a 2D circle with various transformation and rendering functionalities.
+ */
+
 #include "Circle.h"
 #include "PrimitiveRenderer.h"
 
+ /**
+  * @brief Constructs a Circle object with specified color, center position, and radius.
+  *
+  * @param r Red component of the circle's color.
+  * @param g Green component of the circle's color.
+  * @param b Blue component of the circle's color.
+  * @param an Alpha (transparency) component of the circle's color.
+  * @param x X-coordinate of the circle's center.
+  * @param y Y-coordinate of the circle's center.
+  * @param R Radius of the circle.
+  */
 Circle::Circle(Uint8 r, Uint8 g, Uint8 b, Uint8 an, unsigned int x, unsigned int y, unsigned int R)
-	: r{ r }, g{ g }, b{ b }, an{ an }, circleCenter{ x,y }, R{ R }
+    : r{ r }, g{ g }, b{ b }, an{ an }, circleCenter{ x, y }, R{ R }
 {
 
 }
 
+/**
+ * @brief Draws the circle on the screen.
+ *
+ * Uses the PrimitiveRenderer to render the circle's outline.
+ */
 void Circle::draw()
 {
-	PrimitiveRenderer::draw_circle(r,g,b,an,circleCenter.getCoordinates('X'),circleCenter.getCoordinates('Y'),R);
+    PrimitiveRenderer::draw_circle(r, g, b, an, circleCenter.getCoordinates('X'), circleCenter.getCoordinates('Y'), R);
 }
 
-void Circle::fill(SDL_Color fillColor,SDL_Color boundryColor)
+/**
+ * @brief Fills the circle with a specified color and boundary color.
+ *
+ * Uses the boundary fill algorithm provided by the PrimitiveRenderer.
+ *
+ * @param fillColor Fill color for the circle.
+ * @param boundryColor Boundary color for the circle.
+ */
+void Circle::fill(SDL_Color fillColor, SDL_Color boundryColor)
 {
-	PrimitiveRenderer::RiteracyjnyBoundaryFill(Engine::getInstance()->getRenderer(),circleCenter.getCoordinates('X'),circleCenter.getCoordinates('Y'),fillColor,boundryColor);
+    PrimitiveRenderer::RiteracyjnyBoundaryFill(Engine::getInstance()->getRenderer(), circleCenter.getCoordinates('X'), circleCenter.getCoordinates('Y'), fillColor, boundryColor);
 }
 
+/**
+ * @brief Translates (moves) the circle by a specified offset.
+ *
+ * @param tx Offset along the x-axis.
+ * @param ty Offset along the y-axis.
+ */
 void Circle::translate(int tx, int ty)
 {
-	circleCenter.setCoordinates(circleCenter.getCoordinates('X') + tx, circleCenter.getCoordinates('Y') - ty);
+    circleCenter.setCoordinates(circleCenter.getCoordinates('X') + tx, circleCenter.getCoordinates('Y') - ty);
 }
 
+/**
+ * @brief Rotates the circle's center around the origin by a specified angle.
+ *
+ * @param alfa Angle of rotation in degrees.
+ */
 void Circle::rotate(double alfa)
 {
-	// ?
-	double alfaRad = alfa * M_PI / 180.0;
-	
-	int x_point = circleCenter.getCoordinates('X') + R * cos(alfaRad);
-	int y_point = circleCenter.getCoordinates('Y') + R * sin(alfaRad);
-	circleCenter.setCoordinates(x_point, y_point);
+    double alfaRad = alfa * M_PI / 180.0;
+
+    int x_point = circleCenter.getCoordinates('X') + R * cos(alfaRad);
+    int y_point = circleCenter.getCoordinates('Y') + R * sin(alfaRad);
+    circleCenter.setCoordinates(x_point, y_point);
 }
 
+/**
+ * @brief Rotates the circle's center around a specified point by a given angle.
+ *
+ * @param alfa Angle of rotation in degrees.
+ * @param x X-coordinate of the rotation point.
+ * @param y Y-coordinate of the rotation point.
+ */
 void Circle::rotatePoint(double alfa, int x, int y)
 {
-	// ?
-	double alfaRad = alfa * M_PI / 180.0;
+    double alfaRad = alfa * M_PI / 180.0;
 
-	int x_point = circleCenter.getCoordinates('X');
-	int y_point = circleCenter.getCoordinates('Y');
+    int x_point = circleCenter.getCoordinates('X');
+    int y_point = circleCenter.getCoordinates('Y');
 
-	int x_rotated = x_point + (x - x_point) * cos(alfaRad) - (y - y_point) * sin(alfaRad);
-	int y_rotated = y_point + (x - x_point) * sin(alfaRad) + (y - y_point) * cos(alfaRad);
-	circleCenter.setCoordinates(x_rotated, y_rotated);
-
+    int x_rotated = x_point + (x - x_point) * cos(alfaRad) - (y - y_point) * sin(alfaRad);
+    int y_rotated = y_point + (x - x_point) * sin(alfaRad) + (y - y_point) * cos(alfaRad);
+    circleCenter.setCoordinates(x_rotated, y_rotated);
 }
 
+/**
+ * @brief Scales the circle uniformly by a specified factor.
+ *
+ * @param k Scaling factor. A value greater than 1 enlarges the circle, while a value less than 1 shrinks it.
+ */
 void Circle::scale(double k)
 {
-	R = R * k;
+    R = R * k;
 }
 
+/**
+ * @brief Scales the circle around a specified point by a given factor.
+ *
+ * This method adjusts both the circle's center position and its radius.
+ *
+ * @param k Scaling factor. A value greater than 1 enlarges the circle, while a value less than 1 shrinks it.
+ * @param x X-coordinate of the scaling point.
+ * @param y Y-coordinate of the scaling point.
+ */
 void Circle::scalePoint(double k, int x, int y)
 {
-	int x_point = circleCenter.getCoordinates('X');
-	int y_point = circleCenter.getCoordinates('Y');
+    int x_point = circleCenter.getCoordinates('X');
+    int y_point = circleCenter.getCoordinates('Y');
 
-	int x2_point = x_point * k + (1 - k) * x;
-	int y2_point = y_point * k + (1 - k) * y;
+    int x2_point = x_point * k + (1 - k) * x;
+    int y2_point = y_point * k + (1 - k) * y;
 
-	circleCenter.setCoordinates(x2_point, y2_point);
-	R *= k;
-
+    circleCenter.setCoordinates(x2_point, y2_point);
+    R *= k;
 }
--- Circle.h
+++ Circle.h
@@ -1,20 +1,97 @@
+/**
+ * @file Circle.h
+ * @brief Declaration of the Circle class, representing a 2D circle.
+ */
+
 #pragma once
 #include "ShapeObject.h"
 #include "Point2D.h"
+
+ /**
+  * @class Circle
+  * @brief Represents a 2D circle with functionalities for rendering and transformations.
+  *
+  * The Circle class inherits from ShapeObject and provides methods to draw, fill, and transform
+  * a circle object, such as translation, rotation, and scaling.
+  */
 class Circle : public ShapeObject
 {
 public:
-	Circle(Uint8 r, Uint8 g, Uint8 b, Uint8 an, unsigned int x, unsigned int y, unsigned int R);
-	void draw() override;
-	void fill(SDL_Color fillColor, SDL_Color boundryColor) override;
-	void translate(int tx, int ty) override;
-	void rotate(double alfa) override;
-	void rotatePoint(double alfa, int x, int y) override;
-	void scale(double k) override;
-	void scalePoint(double k, int x, int y) override;
-private:
-	Point2D circleCenter;
-	unsigned int R;
-	Uint8 r, g, b, an;
-};
+    /**
+     * @brief Constructs a Circle object with specified color, center position, and radius.
+     *
+     * @param r Red component of the circle's color.
+     * @param g Green component of the circle's color.
+     * @param b Blue component of the circle's color.
+     * @param an Alpha (transparency) component of the circle's color.
+     * @param x X-coordinate of the circle's center.
+     * @param y Y-coordinate of the circle's center.
+     * @param R Radius of the circle.
+     */
+    Circle(Uint8 r, Uint8 g, Uint8 b, Uint8 an, unsigned int x, unsigned int y, unsigned int R);
 
+    /**
+     * @brief Draws the circle on the screen.
+     *
+     * This method renders the circle's outline using the appropriate rendering functions.
+     */
+    void draw() override;
+
+    /**
+     * @brief Fills the circle with a specified color and boundary color.
+     *
+     * @param fillColor The fill color for the circle.
+     * @param boundryColor The boundary color for the circle.
+     */
+    void fill(SDL_Color fillColor, SDL_Color boundryColor) override;
+
+    /**
+     * @brief Translates (moves) the circle by a specified offset.
+     *
+     * @param tx Offset along the x-axis.
+     * @param ty Offset along the y-axis.
+     */
+    void translate(int tx, int ty) override;
+
+    /**
+     * @brief Rotates the circle's center around the origin by a specified angle.
+     *
+     * @param alfa Angle of rotation in degrees.
+     */
+    void rotate(double alfa) override;
+
+    /**
+     * @brief Rotates the circle's center around a specified point by a given angle.
+     *
+     * @param alfa Angle of rotation in degrees.
+     * @param x X-coordinate of the rotation point.
+     * @param y Y-coordinate of the rotation point.
+     */
+    void rotatePoint(double alfa, int x, int y) override;
+
+    /**
+     * @brief Scales the circle uniformly by a specified factor.
+     *
+     * @param k Scaling factor. A value greater than 1 enlarges the circle, while a value less than 1 shrinks it.
+     */
+    void scale(double k) override;
+
+    /**
+     * @brief Scales the circle around a specified point by a given factor.
+     *
+     * This method adjusts both the circle's center position and its radius.
+     *
+     * @param k Scaling factor. A value greater than 1 enlarges the circle, while a value less than 1 shrinks it.
+     * @param x X-coordinate of the scaling point.
+     * @param y Y-coordinate of the scaling point.
+     */
+    void scalePoint(double k, int x, int y) override;
+
+private:
+    Point2D circleCenter; /**< Center point of the circle. */
+    unsigned int R;       /**< Radius of the circle. */
+    Uint8 r;              /**< Red component of the circle's color. */
+    Uint8 g;              /**< Green component of the circle's color. */
+    Uint8 b;              /**< Blue component of the circle's color. */
+    Uint8 an;             /**< Alpha (transparency) component of the circle's color. */
+};
